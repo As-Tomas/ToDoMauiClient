e177427f8c6708fef481583b4a81c0a6efb5ebbb
Tomas Bance <kalianasu@gmail.com>
2022-12-30 10:55:07 +0100

    ManagePage code-behind

--- ToDoMauiClient/Pages/ManageToDoPage.xaml.cs
+++ ToDoMauiClient/Pages/ManageToDoPage.xaml.cs
@@ -1,9 +1,37 @@
+using ToDoMauiClient.DataServices;
+using ToDoMauiClient.Models;
+
 namespace ToDoMauiClient.Pages;
 
 public partial class ManageToDoPage : ContentPage
 {
-	public ManageToDoPage()
+    private readonly IRestDataService _dataService;
+	ToDo _toDo;
+	bool _isNew;
+
+	public ToDo ToDO
+	{
+		get => _toDo;
+		set
+		{
+			_isNew = IsNew(value);
+			_toDo = value;
+			OnPropertyChanged();
+		}
+	}
+
+    public ManageToDoPage(IRestDataService dataService)
 	{
 		InitializeComponent();
+
+		_dataService = dataService;
+		BindingContext = this;
+	}
+
+	bool IsNew(ToDo toDo)
+	{
+		if (toDo.Id == 0)
+			return true;
+		return false;
 	}
 }
