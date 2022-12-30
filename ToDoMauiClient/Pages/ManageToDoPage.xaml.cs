c8a90124153d9839d464d00fc4d95be196f8d7b6
Tomas Bance <kalianasu@gmail.com>
2022-12-30 11:04:38 +0100

    QueryProperty

--- ToDoMauiClient/Pages/ManageToDoPage.xaml.cs
+++ ToDoMauiClient/Pages/ManageToDoPage.xaml.cs
@@ -1,15 +1,17 @@
+using System.Diagnostics;
 using ToDoMauiClient.DataServices;
 using ToDoMauiClient.Models;
 
 namespace ToDoMauiClient.Pages;
 
+[QueryProperty(nameof(ToDo), "ToDo")]
 public partial class ManageToDoPage : ContentPage
 {
     private readonly IRestDataService _dataService;
 	ToDo _toDo;
 	bool _isNew;
 
-	public ToDo ToDO
+	public ToDo ToDo
 	{
 		get => _toDo;
 		set
@@ -33,5 +35,31 @@ public partial class ManageToDoPage : ContentPage
 		if (toDo.Id == 0)
 			return true;
 		return false;
+	}
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+		if (_isNew)
+		{
+			Debug.WriteLine("----> Add new Item");
+			await _dataService.AddToDoAsync(ToDo);
+		}
+		else
+		{
+            Debug.WriteLine("----> Update an Item");
+            await _dataService.UpdateToDoAsync(ToDo);
+        }
+        
+        await Shell.Current.GoToAsync("..");
+    }
+
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+	{
+		await _dataService.DeleteToDoAsync(ToDo.Id);
+        await Shell.Current.GoToAsync("..");
+    }
+
+	async void OnCancelButtonClicked(object sender, EventArgs e)
+	{
+		await Shell.Current.GoToAsync("..");
 	}
 }
